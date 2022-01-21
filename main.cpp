e3031a292800d6d649288197555c6f5a98266fa5
Igor <you@example.com>
2022-01-21 17:37:33 +0300

    working version

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <string>
 #include <iostream>
 #include <mutex>
-#include <functional>
 #include <thread>
-#include <vector>
 #include <queue>
 #include <condition_variable>
 #include <atomic>
 #include <chrono>
 using namespace std::chrono_literals;
 
-constexpr static const unsigned NP=10;
+constexpr unsigned NP=10;
 
 class Task
 {
@@ -18,14 +16,15 @@ public:
     Task(unsigned id=0, unsigned prio=5, std::chrono::milliseconds repeat_after=0s):m_active(true), m_id(id), m_priority(prio), m_repeat_after(repeat_after)
     {
         if (prio<1 || prio > NP)
-            throw std::invalid_argument ("wrong priority value");
+            throw std::invalid_argument ("wrong getPriority value");
     }
-    void Execute()const {std::cout << "task# " + std::to_string(m_id) + ":" + std::to_string(priority()) + "\n";}
+    void Execute()const {std::cout << "task# " + std::to_string(m_id) + ":" + std::to_string(getPriority()) + "\n";}
     void Cancel() {m_active = false;} 
-    bool active()const{return m_active;}
-    unsigned priority()const {return m_priority;}
+    bool isActive()const{return m_active;}
+    unsigned getPriority()const {return m_priority;}
+    bool isPeriodic()const {return 0 != m_repeat_after.count();}
     std::chrono::milliseconds repeat_after()const {return m_repeat_after;}
-    bool periodic()const {return 0 != m_repeat_after.count();}
+
 protected:
     bool      m_active;
     unsigned  m_id;
@@ -39,12 +38,12 @@ class TaskQueue
 public:
     void enqueue(T&& task)
     {
-        os[task.priority()-1].emplace_back(std::move(task));
+        os[task.getPriority()-1].emplace_back(std::move(task));
     }
  
     void bypass(T&& task)
     {
-        os[task.priority()-1].emplace_front(std::move(task));
+        os[task.getPriority()-1].emplace_front(std::move(task));
     }
     
     bool empty() const
@@ -74,23 +73,21 @@ private:
 class Scheduler
 {
 public:
-    explicit Scheduler(bool initial_state=false, unsigned pool_size=0);
+    explicit Scheduler(unsigned pool_size=0);
     ~Scheduler();
     void Enqueue(Task&& task);
-    void Start();
 protected:
     void service();
 
 private:
     std::mutex               m_lock;
     std::condition_variable  m_cond;
-    std::atomic<bool>        m_active;
 
     std::vector<std::thread> m_workers;
     TaskQueue<Task, NP>      m_queue;
 };
 
-Scheduler::Scheduler(bool initial_state, unsigned pool_size) : m_lock(), m_cond(), m_active(initial_state)
+Scheduler::Scheduler(unsigned pool_size)
 {
     for (unsigned i = 0, k = pool_size ? pool_size : std::thread::hardware_concurrency(); i < k; ++i)
         m_workers.push_back(std::thread(&Scheduler::service, this));
@@ -112,35 +109,25 @@ void Scheduler::Enqueue(Task&& task)
     m_cond.notify_one();
 }
 
-void Scheduler::Start()
-{
-    std::unique_lock<std::mutex> lock(m_lock);
-    m_active=true;
-    lock.unlock();
-    m_cond.notify_all();
-}
-
 void Scheduler::service()
 {
-    Task task;
-    
     for (;;)
     {
         std::unique_lock<std::mutex> lock(m_lock);
 
-        m_cond.wait(lock, [this]() {return !m_queue.empty() && m_active;});
+        m_cond.wait(lock, [this]() {return !m_queue.empty();});
 
         auto task = m_queue.next();
         
-        lock.unlock(); //unlock the queue
+        lock.unlock(); 
 
-        if (task.active())
+        if (task.isActive())
         {
             task.Execute();
 
-            if (task.periodic()) 
+            if (task.isPeriodic()) 
             {
-                while (m_active && task.active())
+                while (task.isActive())
                 {
                     std::this_thread::sleep_for(task.repeat_after());
                     task.Execute();
@@ -160,9 +147,8 @@ int main()
         sched.Enqueue({i, i%10+1});
     }
 
-    sched.Start();
     sched.Enqueue({777, 9, 1s});
-    sched.Enqueue({666,1,100ms});
+    sched.Enqueue({666, 1, 100ms});
 
     std::this_thread::sleep_for(2s);
     
